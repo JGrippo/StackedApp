108c93fe887041ed5c6d2b6060ec00ffd54dec5e
Keenan Baynard <ksbaynard@hotmail.com>
2018-06-05 10:06:19 -0400

    add stack

--- StackedAngular/src/app/app.module.ts
+++ StackedAngular/src/app/app.module.ts
@@ -2,10 +2,12 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { StackComponent } from './stack/stack.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    StackComponent
   ],
   imports: [
     BrowserModule
