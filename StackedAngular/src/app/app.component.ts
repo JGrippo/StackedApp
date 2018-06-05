5b5393afa3baf3115d7205988bf7cd0158ff22fd
Keenan Baynard <ksbaynard@hotmail.com>
2018-06-05 15:17:33 -0400

    update

--- StackedAngular/src/app/app.component.ts
+++ StackedAngular/src/app/app.component.ts
@@ -4,16 +4,7 @@ import { HeaderComponent } from './components/header/header.component';
 
 @Component({
   selector: 'app-root',
-  template: `
-  <!-- header  -->
-  <app-header></app-header>
-
-  <!-- routes will be rendered here -->
-  <app-stack></app-stack>
-
-  <!-- footer -->
-  <app-footer></app-footer>
-`,
+  templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
